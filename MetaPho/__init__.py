5c89455e1bacf25394b13a6d0557935dcf719a88
Akkana Peck <akkana@shallowsky.com>
2013-02-09 12:56:08 -0800

    Add key and focus handling to add new tags. It's actually usable now (barely) for real image tagging.

--- MetaPho/__init__.py
+++ MetaPho/__init__.py
@@ -58,15 +58,29 @@ class Tagger(object) :
     '''
 
     def __init__(self) :
-        '''imagelist: a list of MetaPho Images'''
-        pass
+        '''tagger: an object to manage MetaPho image tags'''
+        self.tagfiles = []
+        self.commondir = None
+
+        # Have any tags changed during this run?
+        # Don't update the Tags file if the user doesn't change anything.
+        self.changed = False
 
     def __repr__(self) :
         '''Returns a string summarizing all known images and tags,
            suitable for printing on stdout or pasting into a Tags file.
         '''
+        # outstr = "Tag files read: "
+        # outstr += ' '.join(self.tagfiles)
+        # outstr += '\n'
+
+        # outstr += "Common dir: " + self.commondir + "\n"
+
         outstr = ''
         for tagno, tagstr in enumerate(Tagger.gTagList) :
+            # No empty tags
+            if tagstr.strip() == '' :
+                continue
             outstr += "tag %s :" % tagstr
             for img in Image.gImageList :
                 if tagno in img.tags :
@@ -75,11 +89,42 @@ class Tagger(object) :
 
         return outstr
 
+    def writeTagFile(self) :
+        '''Save the current set of tags to a Tags file chosen from
+           the top-level directory used in the images we've seen.
+           If there was a previous Tags file there, it will be saved
+           as Tags.bak.
+        '''
+        if not self.changed :
+            print "No tags changed; not rewriting Tags file"
+            return
+
+        outpath = os.path.join(self.commondir, "Tags")
+        print "Saving to", outpath
+        if os.path.exists(outpath) :
+            os.rename(outpath, outpath + ".bak")
+        outfile = open(outpath, "w")
+        outfile.write(str(self))
+        outfile.close()
+
     def readTags(self, dirname) :
         '''Read in tags from files named in the given directory,
            and tag images in the imagelist appropriately.
            Tags will be appended to Tagger.gTagList.
         '''
+        # Keep track of the dir common to all directories we use:
+        if self.commondir == None :
+            print "======= Initializing commondir to", dirname
+            self.commondir = dirname
+        else :
+            print "======= What's common to", self.commondir, "and", dirname, "?"
+            self.commondir = os.path.commonprefix([self.commondir, dirname])
+                # commonpre has a bug, see
+                # http://rosettacode.org/wiki/Find_common_directory_path#Python
+                # but this causes other problems:
+                # .rpartition(os.path.sep)[0]
+            print "Commondir becomes", self.commondir
+
         # Might want to be recursive and use os.walk ...
         # or maybe go the other way, search for Tags files
         # *above* the current directory but not below.
@@ -99,11 +144,12 @@ tag Denmark: p103050.jpg, p103051.jpg
         try :
             pathname = os.path.join(dirname, "Tags")
             fp = open(pathname)
-            print "Opened", pathname
+            self.tagfiles.append(pathname)
         except IOError :
             try :
                 pathname = os.path.join(dirname, "Keywords")
                 fp = open(pathname)
+                self.tagfiles.append(pathname)
             except IOError :
                 print "No Tags file in", dirname
                 return
@@ -170,6 +216,8 @@ tag Denmark: p103050.jpg, p103051.jpg
            Return the index (in the global tags list) of the tag just added,
            or None if error.
         '''
+        self.changed = True
+
         if type(tag) is int :
             if tag not in img.tags :
                 img.tags.append(tag)
@@ -185,6 +233,8 @@ tag Denmark: p103050.jpg, p103051.jpg
 
     def toggleTag(self, tagno, img) :
         '''Toggle tag number tagno for the given img.'''
+        self.changed = True
+
         if tagno in img.tags :
             img.tags.remove(tagno)
             return
