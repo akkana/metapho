e0fc6cb6862547aa5f2f8d7fc542646aac30e0f6
Akkana Peck <akkana@shallowsky.com>
2016-12-04 16:01:10 -0700

    Correct capitalization in a docstring

--- metapho/__init__.py
+++ metapho/__init__.py
@@ -4,7 +4,7 @@
 
 # Copyright 2013,2016 by Akkana Peck: share and enjoy under the GPL v2 or later.
 
-'''MetaPho: an image tagger and viewer.'''
+'''metapho: an image tagger and viewer.'''
 
 __version__ = "0.6"
 __author__ = "Akkana Peck <akkana@shallowsky.com>"
