ab3f31c3880a96d5730ab7051325701814a2fb03
Akkana Peck <akkana@shallowsky.com>
2024-11-28 15:18:47 -0700

    Update ImageViewer to work as a commandline app.

--- metapho/gtkpho/ImageViewer.py
+++ metapho/gtkpho/ImageViewer.py
@@ -13,23 +13,26 @@ import gc
 
 from metapho import Image
 
+DEBUG = False
+
 
 class ImageViewer(Gtk.DrawingArea):
     """A simple PyGTK image viewer widget.
     """
 
     def __init__(self):
-        super(ImageViewer, self).__init__()
+        super().__init__()
 
-        # self.connect("expose-event", self.expose_handler)
+        self.cr = None
+
         self.connect("draw", self.draw)
         self.connect("size-allocate", self.detect_resize)
 
         self.pixbuf = None
         self.label_text = None
 
-        self.width = None
-        self.height = None
+        self.width = 0
+        self.height = 0
 
         self.savewidth = None
         self.saveheight = None
@@ -46,25 +49,44 @@ class ImageViewer(Gtk.DrawingArea):
     def get_window_size(self):
         """Return width, height of the current window allocation."""
         # Before the window is created, get_allocation will return 1, 1
-        # which plays havoc with various calculations.
-        if not self.window:
-            return None, None
-        rect = self.get_allocation()
-        return rect.width, rect.height
+        # which plays havoc with other calculations.
+        # print("get_window_size traceback:")
+        # traceback.print_stack()
+        try:
+            rect = self.get_allocation()
+            if DEBUG:
+                print("get_window_size returning %dx%d"
+                      % (rect.width, rect.height))
+            # Early on, GTK gives us a bogus 1x1 window size,
+            # which messes things up.
+            if rect.width < 10 or rect.height < 10:
+                return 0, 0
+            return rect.width, rect.height
+        except:
+            return 0, 0
 
 
     def detect_resize(self, w, rect):
         self.width, self.height = self.get_window_size()
+        if DEBUG:
+            print("detect_resize to %dx%d" % (self.width, self.height))
         self.prepare_image()
         self.show_image()
 
 
     def draw(self, widget, cr):
         """Draw the image, scaled appropriately."""
-        w, h = self.get_window_size()
-        if w != self.width or h != self.height:
-            self.width = w
-            self.height = h
+        self.cr = cr
+        # w, h = self.get_window_size()
+        # if w != self.width or h != self.height:
+        #     if DEBUG:
+        #         print("draw: window size changed to %dx%d" % (w, h))
+        #     self.width = w
+        #     self.height = h
+        # Calling self.get_window_size() here somehow leads to an
+        # infinite loop. So I guess it's better to rely on detect_resize.
+        w = self.width
+        h = self.height
 
         # Have we had load_image called, but we weren't ready for it?
         # Now, theoretically, we are ... so call it again.
@@ -73,14 +95,12 @@ class ImageViewer(Gtk.DrawingArea):
 
         self.show_image(cr)
 
-
     # Mapping from EXIF orientation tag to degrees rotated.
     # http://sylvana.net/jpegcrop/exif_orientation.html
     exif_rot_table = [ 0, 0, 180, 180, 270, 270, 90, 90 ]
     # Note that orientations 2, 4, 5 and 7 also involve a flip.
     # We're not implementing that right now, because nobody
     # uses it in practice.
-
 
     def load_image(self, img):
         """Load an image from a filename or metaphe.Image.
@@ -91,7 +111,9 @@ class ImageViewer(Gtk.DrawingArea):
         # which happens before ImageViewer gets its own first draw event.
         # So we don't have a size yet when this is first called.
         if not self.width or not self.height:
-            self.width, self.height = self.get_window_size()
+            if DEBUG:
+                print("load_image with no window size")
+                return
 
         if isinstance(img, Image):
             self.cur_img = img
@@ -101,6 +123,9 @@ class ImageViewer(Gtk.DrawingArea):
         self.label_text = None
 
         if not self.window or not self.width or not self.height:
+            if DEBUG:
+                print("Oops, window", self.window, "width", self.width,
+                      "height", self.height)
             return 0
 
         if self.cur_img:
@@ -119,8 +144,25 @@ class ImageViewer(Gtk.DrawingArea):
            Return 1 for success, 0 for valid image but not ready,
            -1 for invalid image or other error.
         """
+
+        # We can't do any of the rotation until the window appears
+        # so we know our window size.
+        # But we have to load the first pixbuf anyway, because
+        # otherwise we may end up pointing to an image that can't
+        # be loaded. Super annoying! We'll end up reloading the
+        # pixbuf again after the window appears, so this will
+        # slow down the initial window slightly.
+        if DEBUG:
+            print("in prepare_image, size is %dx%d" % (self.width, self.height))
+        if not self.width or not self.height:
+            if DEBUG:
+                print("prepare_image called with no window size")
+            return -1
+
         if not self.cur_img:
-            return
+            if DEBUG:
+                print("No current image")
+            return -1
 
         self.label_text = None
 
@@ -131,16 +173,8 @@ class ImageViewer(Gtk.DrawingArea):
 
         try:
             newpb = GdkPixbuf.Pixbuf.new_from_file(self.cur_img.filename)
-
-            # We can't do any of the rotation until the window appears
-            # so we know our window size.
-            # But we have to load the first pixbuf anyway, because
-            # otherwise we may end up pointing to an image that can't
-            # be loaded. Super annoying! We'll end up reloading the
-            # pixbuf again after the window appears, so this will
-            # slow down the initial window slightly.
-            if not self.width:
-                return True
+            if DEBUG:
+                print("Got a new pixbuf from", self.cur_img.filename)
 
             # Do we need to check rotation info for this image?
             # Get the EXIF embedded rotation info.
@@ -184,6 +218,11 @@ class ImageViewer(Gtk.DrawingArea):
                         newh = oldh * self.width / oldw
 
                 # Finally, do the scale:
+                if DEBUG:
+                    print("window size: %dx%d" % (self.width, self.height))
+                    print("image size: %dx%d" % (oldw, oldh))
+                    print("pixbuf size: %dx%d" % (oldw, oldh))
+                    print("scaling to: %dx%d" % (neww, newh))
                 newpb = newpb.scale_simple(neww, newh,
                                            GdkPixbuf.InterpType.BILINEAR)
 
@@ -198,8 +237,9 @@ class ImageViewer(Gtk.DrawingArea):
             loaded = True
 
         except gi.repository.GLib.Error as e:
-            # print("Error reading image " + self.cur_img.filename)
-            # print(e)
+            if DEBUG:
+                print("Error reading image " + self.cur_img.filename)
+                print(e)
             print("Skipping %s: not an image" % self.cur_img.filename)
             self.pixbuf = None
             loaded = False
@@ -209,23 +249,42 @@ class ImageViewer(Gtk.DrawingArea):
         newpb = None
         gc.collect()
 
-        return loaded
+        if loaded:
+            self.show_image()
+            return 1
+        else:
+            return 0
 
 
     def show_image(self, cr=None):
-        if not self.pixbuf or not self.window:
+        if not self.window:
+            if DEBUG:
+                print("No window yet, can't show image")
+            return
+        if not self.pixbuf:
+            if DEBUG:
+                print("No pixbuf, can't show image")
             return
 
         if not cr:
-            cr = self.window.cairo_create()
+            cr = self.cr
+        if not cr:
+            if DEBUG:
+                print("eek, can't show image without cairo context")
+            return
         self.clear(cr)
 
         # Center the image:
         x = (self.width - self.pixbuf.get_width()) / 2
         y = (self.height - self.pixbuf.get_height()) / 2
 
+        self.clear(cr)
         Gdk.cairo_set_source_pixbuf(cr, self.pixbuf, x, y)
         cr.paint()
+
+        # But cairo doesn't actually draw anything unless
+        # you force it to:
+        self.queue_draw()
 
 
     def clear(self, cr):
@@ -300,11 +359,12 @@ class ImageViewerWindow(Gtk.Window):
         # We don't seem to need the destroy event
         # self.connect("destroy", self.destroy)
 
-        self.set_key_handler(self.key_press_event)
+        self.connect("key-press-event", self.key_press_event, self)
 
         self.main_vbox = Gtk.VBox(spacing=8)
 
         self.viewer = ImageViewer()
+        self.viewer.window = self
         self.viewer.set_size_request(self.width, self.height)
         self.viewer.resize_fn =  self.resize_fn
         self.main_vbox.pack_start(self.viewer, True, True, 0)
@@ -325,9 +385,6 @@ class ImageViewerWindow(Gtk.Window):
         self.show_all();
         Gtk.main()
 
-    def set_key_handler(self, fcn):
-        self.connect("key-press-event", fcn, self)
-
     def add_image(self, img):
         if not isinstance(img, Image):
             img = Image(img)
@@ -343,19 +400,39 @@ class ImageViewerWindow(Gtk.Window):
 
     def next_image(self):
         if not self.img_list:
+            if DEBUG:
+                print("No img_list")
             return
-        self.imgno = (self.imgno + 1) % len(self.img_list)
-        self.viewer.load_image(self.img_list[self.imgno])
-        self.viewer.show_image()
+        if DEBUG:
+            print("Going from", self.imgno, "->", self.img_list[self.imgno])
+        while True:
+            self.imgno += 1
+            if self.imgno >= len(self.img_list):
+                self.imgno = len(self.img_list) - 1
+                if DEBUG:
+                    print("Can't go beyond last image")
+                return
+            if DEBUG:
+                print("  to", self.imgno, "->", self.img_list[self.imgno])
+            loaded = self.viewer.load_image(self.img_list[self.imgno])
+            if loaded == 1:
+                self.viewer.show_image()
+                return
 
     def prev_image(self):
         if not self.img_list:
             return
-        self.imgno = (self.imgno - 1)
-        if self.imgno < 0:
-            self.imgno = 0
-        self.viewer.load_image(self.img_list[self.imgno])
-        self.viewer.show_image()
+        while True:
+            self.imgno -= 1
+            if self.imgno < 0:
+                self.imgno = 0
+                if DEBUG:
+                    print("Can't go before first image")
+                return
+            loaded = self.viewer.load_image(self.img_list[self.imgno])
+            if loaded == 1:
+                self.viewer.show_image()
+                return
 
     def quit(self):
         Gtk.main_quit()
@@ -382,7 +459,7 @@ class ImageViewerWindow(Gtk.Window):
         if event.string == " ":
             self.next_image()
             return
-        if event.keyval == Gtk.keysyms.BackSpace:
+        if event.keyval == Gdk.KEY_BackSpace:
             self.prev_image()
             return
         if event.string == "f":
@@ -399,7 +476,12 @@ class ImageViewerWindow(Gtk.Window):
 def main():
     import sys
     win = ImageViewerWindow(sys.argv[1:], exit_on_q=True)
-    win.run()
+    try:
+        win.run()
+    except KeyboardInterrupt:
+        if DEBUG:
+            print("Ctrl-C")
+        win.quit()
 
 
 if __name__ == "__main__":
