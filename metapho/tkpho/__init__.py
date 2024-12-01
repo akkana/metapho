c43af033c7f83b06cfacfcd9d2aecaaa01c40c53
Akkana Peck <akkana@shallowsky.com>
2024-12-01 15:52:16 -0700

    tkpho branch: add a TK-based ImageViewer class which will also eventually become a replacement for pho

---
+++ metapho/tkpho/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+# ImageViewer: a lightweight standalone image viewer.
+
+# Part of metapho: an image tagger and viewer.
+
+# Copyright 2024 by Akkana Peck: share and enjoy under the GPL v2 or later.
+
+'''metapho.tkpho: TkInter-based display functions for metapho.'''
+
+from .tkpho import *
+from .ImageViewer import *
